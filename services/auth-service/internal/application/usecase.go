cf00f75614407638737a30061daba4eb4a0a70c1
waste3d <kolya@waste3d.su>
2025-11-21 21:36:51 +0300

    feat: funcs

--- services/auth-service/internal/application/usecase.go
+++ services/auth-service/internal/application/usecase.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"context"
+	"errors"
 	"gameplatform/services/auth-service/internal/domain"
 	"gameplatform/services/auth-service/internal/infrastructure/cache"
 	"gameplatform/services/auth-service/internal/infrastructure/repository"
@@ -52,4 +53,54 @@ func (uc *AuthUseCase) Register(ctx context.Context, username, email, password s
 	}
 
 	return user.ID.String(), nil
+}
+
+func (uc *AuthUseCase) Login(ctx context.Context, email, password string) (string, string, error) {
+	user, err := uc.userRepo.GetByEmail(ctx, email)
+	if err != nil {
+		return "", "", errors.New("invalid credentials")
+	}
+
+	if err := uc.hasher.Compare(user.Password, password); err != nil {
+		return "", "", errors.New("invalid credentials")
+	}
+
+	return uc.generateAndSaveTokens(ctx, user.ID.String())
+}
+
+func (uc *AuthUseCase) Refresh(ctx context.Context, oldRefreshToken string) (string, string, error) {
+	userID, err := uc.tokenManager.ValidateRefreshToken(oldRefreshToken)
+	if err != nil {
+		return "", "", errors.New("invalid refresh token")
+	}
+	cachedUserID, err := uc.tokenCache.CheckRefresh(ctx, oldRefreshToken)
+	if err != nil || cachedUserID != userID {
+		return "", "", errors.New("refresh token revoked or expired")
+	}
+
+	_ = uc.tokenCache.DeleteRefresh(ctx, oldRefreshToken)
+
+	return uc.generateAndSaveTokens(ctx, userID)
+}
+
+func (uc *AuthUseCase) Logout(ctx context.Context, refreshToken string) error {
+	return uc.tokenCache.DeleteRefresh(ctx, refreshToken)
+}
+
+func (uc *AuthUseCase) ValidateAccess(token string) (string, error) {
+	return uc.tokenManager.ValidateAccessToken(token)
+}
+
+func (uc *AuthUseCase) generateAndSaveTokens(ctx context.Context, userID string) (string, string, error) {
+	access, refresh, err := uc.tokenManager.Generate(userID)
+	if err != nil {
+		return "", "", err
+	}
+
+	// Сохраняем refresh в Redis
+	if err := uc.tokenCache.SaveRefresh(ctx, userID, refresh); err != nil {
+		return "", "", err
+	}
+
+	return access, refresh, nil
 }
