ec734a46ed6bfc0ad3b9e047271e5602c212f0f1
waste3d <kolya@waste3d.su>
2025-11-27 18:21:33 +0300

    feat: payment service))

--- services/auth-service/internal/domain/user.go
+++ services/auth-service/internal/domain/user.go
@@ -13,10 +13,10 @@ var (
 )
 
 type User struct {
-	ID        uuid.UUID
-	Email     string
-	Username  string
-	Password  string
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Email     string    `gorm:"uniqueIndex;not null"` // <--- Вот это создаст уникальность
+	Username  string    `gorm:"uniqueIndex;not null"` // Скорее всего, юзернейм тоже нужен уникальный?
+	Password  string    `gorm:"not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
